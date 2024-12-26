723450a48dbfe6eaa754077dec07db2dfda2525a
zennyawjkthwkthjkqwtqjk36tlkqt <zentixal1@gmail.com>
2024-12-26 20:32:41 +0600

    Update ZensHubMain.lua

--- ZensHubMain.lua
+++ ZensHubMain.lua
@@ -1,1 +1,2 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+script_key="QsKnjArimYhiurbrzHKaGmZKaJufUgxg";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/55fedb08e3f3641f0a031b432da772cf.lua"))()
